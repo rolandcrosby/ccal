ee0d61fff1e42a650471576c3968c68223ec1214
Roland Crosby <rcrosby@mediamath.com>
2015-06-12 11:32:14 -0400

    add command line behavior

--- ccal.py
+++ ccal.py
@@ -1,18 +1,43 @@
+"""Usage:
+    ccal
+    ccal <year>
+    ccal <month> <year>"""
+
 import datetime
 import calendar
 import sys
 import os
+from docopt import docopt
 
-rows = int(os.popen('stty size', 'r').read().split()[0])
-if rows == 0:
-    rows = 25
-today = datetime.date.today()
-day = today - datetime.timedelta(today.isoweekday() % 7)
-end = day + datetime.timedelta(7 * (rows - 1))
-while day < end:
-    sys.stdout.write('{:>3}'.format(day.day))
-    if day.isoweekday() == 6:
-        if day.day < 8:
-            sys.stdout.write('  {0}'.format(calendar.month_name[day.month]))
-        sys.stdout.write('\n')
-    day += datetime.timedelta(1)
+def write_cal(start, end, dest=sys.stdout):
+    day = start - datetime.timedelta(start.isoweekday() % 7)
+    end = end + datetime.timedelta(7 - end.isoweekday())
+    while day < end:
+        dest.write('{:>3}'.format(day.day))
+        if day.isoweekday() == 6:
+            if day.day < 8:
+                dest.write('  {0}'.format(calendar.month_name[day.month]))
+            dest.write('\n')
+        day += datetime.timedelta(1)
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    print args
+    if args['<year>'] is not None:
+        year = int(args['<year>'])
+        if args['<month>'] is not None:
+            month = int(args['<month>'])
+            start = datetime.date(year, month, 1)
+            end = datetime.date(year, month, calendar.monthrange(year, month)[1])
+        else:
+            start = datetime.date(year, 1, 1)
+            end = datetime.date(year, 12, 31)
+    else:
+        rows = int(os.popen('stty size', 'r').read().split()[0])
+        if rows == 0:
+            rows = 25
+        today = datetime.date.today()
+        start = today - datetime.timedelta(today.isoweekday() % 7)
+        end = start + datetime.timedelta(7 * (rows - 1))
+    write_cal(start, end)
+
